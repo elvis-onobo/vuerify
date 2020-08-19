77fee6c0e8664654978f1f67b6faa79130bcb2ac
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-08-19 15:56:32 +0100

    Chore: Add Readme

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -3,27 +3,27 @@
     <form>
       <label>
         First Name
-        <input type="text" name="firstname" />
+        <input type="text" name="firstname" v-model.lazy="firstname" />
       </label>
-
+      {{firstname}}
       <label>
         Last Name
-        <input type="text" name="lastname" />
+        <input type="text" name="lastname" v-model.lazy="lastname" />
       </label>
 
       <label>
         Email
-        <input type="text" name="email" />
+        <input type="text" name="email" v-model.lazy="email" />
       </label>
 
       <label>
         Phone Number
-        <input type="text" name="phone" />
+        <input type="text" name="phone" v-model.lazy="phone" />
       </label>
 
       <label>
         Password
-        <input type="text" name="password" />
+        <input type="text" name="password" v-model.lazy="password" />
       </label>
 
       <button>Register</button>
@@ -32,7 +32,17 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: "",
+      password: "",
+    };
+  },
+};
 </script>
 
 <style scoped>
