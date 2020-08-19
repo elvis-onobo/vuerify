0a627127dbcc21ed9dd6da958f1c113fa37d29c4
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-08-19 15:15:43 +0100

    Feat(Register): Add registration form

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <register></register>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Register from "./components/Register.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Register,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form>
+      <label>
+        First Name
+        <input type="text" name="firstname" />
+      </label>
+
+      <label>
+        Last Name
+        <input type="text" name="lastname" />
+      </label>
+
+      <label>
+        Email
+        <input type="text" name="email" />
+      </label>
+
+      <label>
+        Phone Number
+        <input type="text" name="phone" />
+      </label>
+
+      <label>
+        Password
+        <input type="text" name="password" />
+      </label>
+
+      <button>Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
